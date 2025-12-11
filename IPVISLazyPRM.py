923417d095dd3832501aea7eaf1809f1e7baa776
Anton Kisel <anton.kisel@gmx.de>
2025-12-11 08:47:49 +0100

    Lazy laeuft jetzt mit 3D

--- IPVISLazyPRM.py
+++ IPVISLazyPRM.py
@@ -7,15 +7,24 @@ License is based on Creative Commons: Attribution-NonCommercial 4.0 Internationa
 """
 
 import networkx as nx
-
+import matplotlib.pyplot as plt
 
 def lazyPRMVisualize(planner, solution = [] , ax=None, nodeSize = 300):
     graph = planner.graph.copy()
     collChecker = planner._collisionChecker
     collEdges = planner.collidingEdges
     nonCollEdges = planner.nonCollidingEdges
+
+    pos_unnown_dim = nx.get_node_attributes(graph,'pos')
+    first_pos = next(iter(pos_unnown_dim.values()))
+    num_dimensions = len(first_pos)
+
+    if num_dimensions > 2:
+        pos = {node: (coords[0], coords[1]) for node, coords in pos_unnown_dim.items()}
+    else:
+        pos = pos_unnown_dim
+
     # get a list of positions of all nodes by returning the content of the attribute 'pos'
-    pos = nx.get_node_attributes(graph,'pos')
     color = nx.get_node_attributes(graph,'color')
 
     collChecker.drawObstacles(ax)
@@ -87,4 +96,60 @@ def lazyPRMVisualize(planner, solution = [] , ax=None, nodeSize = 300):
 
     
     return
+
+def visibilityPRMVisualizeWspace(planner, solution, ax = None, nodeSize = 100):
+    """ Draw graph, obstacles and solution in a axis environment of matplotib.
+    """
+    # get a list of positions of all nodes by returning the content of the attribute 'pos'
+    graph = planner.graph
+    collChecker = planner._collisionChecker
+
+    collChecker.drawObstacles(ax)
+    
+    
+    pos = nx.get_node_attributes(graph,'pos')
+    # todo extract from pos the first two dimensions only for drawing in workspace
+    pos2D = dict()
+    for key in pos.keys():
+        pos2D[key] = (pos[key][0], pos[key][1])
+        
+    pos = pos2D
+    
+    # draw graph (nodes colorized by degree)
+    nx.draw_networkx_nodes(graph, pos,  cmap=plt.cm.Blues, ax = ax, node_size=nodeSize)
+    nx.draw_networkx_edges(graph,pos,
+                                ax = ax
+                                 )
+    Gcc = sorted(nx.connected_components(graph), key=len, reverse=True)
+    G0=graph.subgraph(Gcc[0])# = largest connected component
+
+    # how largest connected component
+    nx.draw_networkx_edges(G0,pos,
+                               edge_color='b',
+                               width=3.0, ax = ax
+                            )
+
+    
+    # draw nodes based on solution path
+    Gsp = nx.subgraph(graph,solution)
+    nx.draw_networkx_nodes(Gsp,pos,
+                            node_size=nodeSize*1.5,
+                             node_color='g',  ax = ax)
+        
+    # draw edges based on solution path
+    nx.draw_networkx_edges(Gsp,pos,alpha=0.8,edge_color='g',width=10,  ax = ax)
+        
+    # draw start and goal
+    if "start" in graph.nodes(): 
+        nx.draw_networkx_nodes(graph,pos,nodelist=["start"],
+                                   node_size=nodeSize*1.5,
+                                   node_color='#00dd00',  ax = ax)
+        nx.draw_networkx_labels(graph,pos,labels={"start": "S"},  ax = ax)
+
+
+    if "goal" in graph.nodes():
+        nx.draw_networkx_nodes(graph,pos,nodelist=["goal"],
+                                   node_size=nodeSize*1.5,
+                                   node_color='#DD0000',  ax = ax)
+        nx.draw_networkx_labels(graph,pos,labels={"goal": "G"},  ax = ax)
 
